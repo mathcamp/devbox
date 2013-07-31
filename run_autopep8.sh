b9e2c1f2fce287e60f49a1579f37e34baaf6337c
Steven Arcangeli <steven@highlig.ht>
2013-07-31 15:11:27 -0700

    Fixing the autopep8 script

--- run_autopep8.sh
+++ run_autopep8.sh
@@ -6,11 +6,11 @@ main() {
         local _package=$(basename $(readlink -f .))
         find $_package -name '*.py' | xargs $_autopep
     else
-        local _diff=$(git diff --name-only --diff-filter=ACMRT)
+        local _diff=$(git diff --name-only --diff-filter=ACMRT | grep '.py$')
         if [ "$_diff" ]; then
             echo "$_diff" | xargs $_autopep
         fi
-        local _diff_cached=$(git diff --cached --name-only --diff-filter=ACMRT)
+        local _diff_cached=$(git diff --cached --name-only --diff-filter=ACMRT | grep '.py$')
         if [ "$_diff_cached" ]; then
             echo "$_diff_cached" | xargs $_autopep
         fi
